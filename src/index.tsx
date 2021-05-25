c37c568d7d37d00607408292f6d6c94173065bf0
권순관 <reactsaranghe@naver.com>
2021-05-25 12:17:38 +0900

    feat: project setting

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "components/App";
 import reportWebVitals from "./reportWebVitals";
+import GlobalStyle from "style/GlobalStyle";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
---
+++ src/style/GlobalStyle.ts
@@ -0,0 +1,11 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        padding: 0;
+        margin:0;
+        box-sizing: border-box;
+    }
+`;
+
+export default GlobalStyle;
