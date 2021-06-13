8bb983428e55b2d4c3e995c7fecba81028dadd46
권순관 <reactsaranghe@naver.com>
2021-06-14 08:41:16 +0900

    feat: 사전순 정렬 추가

--- src/atom/user.ts
+++ src/atom/user.ts
@@ -3,5 +3,11 @@ import DB from "data/db.json";
 
 export const userAtom = atom({
   key: "userAtom",
-  default: DB,
+  default: DB.sort((a, b): number => {
+    return a.companyName < b.companyName
+      ? -1
+      : a.companyName > b.companyName
+      ? 1
+      : 0;
+  }),
 });
